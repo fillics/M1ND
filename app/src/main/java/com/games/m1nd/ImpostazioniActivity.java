8e2a9d0895be08bdcce952eb6583535c71c5e84a
Filippo Caliò <filippo,calio99@gmail.com>
2020-05-15 22:35:34 +0200

    Aggiunta di pallini per il controllo

---
+++ app/src/main/java/com/games/m1nd/ImpostazioniActivity.java
@@ -0,0 +1,14 @@
+package com.games.m1nd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImpostazioniActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_impostazioni);
+    }
+}
---
+++ app/src/main/java/com/games/m1nd/PartiteRecentiActivity.java
@@ -0,0 +1,14 @@
+package com.games.m1nd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PartiteRecentiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partite_recenti);
+    }
+}
